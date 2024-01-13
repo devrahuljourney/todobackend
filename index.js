58eac95c9b9d313215762f8374989acbf815aca5
rahulkumarv269 <rahulkumarv269@gmail.com>
2024-01-13 21:05:35 +0530

    change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require('cors');
-app.use(cors({ origin: 'https://your-todo-your-reminder.netlify.app/', credentials: true }));
+app.use(cors({ origin: 'https://your-todo-your-reminder.netlify.app', credentials: true }));
 
 const cookieParser = require("cookie-parser");
 
